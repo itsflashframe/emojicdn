b03bb69ff16220a4f2038498c282970362376b2e
itsflashframe <itsflashframe@gmail.com>
2026-04-03 07:56:07 +0100

    Create overrides.js

---
+++ api/overrides.js
@@ -0,0 +1,269 @@
+// MIT License — emoji-datasource: iamcal/emoji-data
+// well thhis one way to fix a bug
+
+
+const overrides = {
+
+  '⏭': {
+    apple:     '23ed-fe0f',   google:    '23ed-fe0f',
+    facebook:  '23ed-fe0f',   twitter:   '23ed-fe0f',
+    messenger: '23ed-fe0f',
+    blobmoji:  'emoji_u23ed', emojitwo:  '23ed',
+    opencolor: '23ED',        openblack: '23ED',
+    oneui:     '23ed',        whatsapp:  '23ed',
+    fluent3d:  'next-track-button', fluentflat: 'next-track-button', fluenthc: 'next-track-button',
+  },
+
+  '⏮': {
+    apple:     '23ee-fe0f',   google:    '23ee-fe0f',
+    facebook:  '23ee-fe0f',   twitter:   '23ee-fe0f',
+    messenger: '23ee-fe0f',
+    blobmoji:  'emoji_u23ee', emojitwo:  '23ee',
+    opencolor: '23EE',        openblack: '23EE',
+    oneui:     '23ee',        whatsapp:  '23ee',
+    fluent3d:  'last-track-button', fluentflat: 'last-track-button', fluenthc: 'last-track-button',
+  },
+
+  '⏯': {
+    apple:     '23ef-fe0f',   google:    '23ef-fe0f',
+    facebook:  '23ef-fe0f',   twitter:   '23ef-fe0f',
+    messenger: '23ef-fe0f',
+    blobmoji:  'emoji_u23ef', emojitwo:  '23ef',
+    opencolor: '23EF',        openblack: '23EF',
+    oneui:     '23ef',        whatsapp:  '23ef',
+    fluent3d:  'play-or-pause-button', fluentflat: 'play-or-pause-button', fluenthc: 'play-or-pause-button',
+  },
+
+  '🖐': {
+    apple:     '1f590-fe0f',  google:    '1f590-fe0f',
+    facebook:  '1f590-fe0f',  twitter:   '1f590-fe0f',
+    messenger: '1f590-fe0f',
+    blobmoji:  'emoji_u1f590', emojitwo: '1f590',
+    opencolor: '1F590',       openblack: '1F590',
+    oneui:     '1f590',       whatsapp:  '1f590',
+    fluent3d:  'hand-with-fingers-splayed', fluentflat: 'hand-with-fingers-splayed', fluenthc: 'hand-with-fingers-splayed',
+  },
+
+  '👁': {
+    apple:     '1f441-fe0f',  google:    '1f441-fe0f',
+    facebook:  '1f441-fe0f',  twitter:   '1f441-fe0f',
+    messenger: '1f441-fe0f',
+    blobmoji:  'emoji_u1f441', emojitwo: '1f441',
+    opencolor: '1F441',       openblack: '1F441',
+    oneui:     '1f441',       whatsapp:  '1f441',
+    fluent3d:  'eye',         fluentflat: 'eye',         fluenthc: 'eye',
+  },
+
+  '☯': {
+    apple:     '262f-fe0f',   google:    '262f-fe0f',
+    facebook:  '262f-fe0f',   twitter:   '262f-fe0f',
+    messenger: '262f-fe0f',
+    blobmoji:  'emoji_u262f', emojitwo:  '262f',
+    opencolor: '262F',        openblack: '262F',
+    oneui:     '262f',        whatsapp:  '262f',
+    fluent3d:  'yin-yang',    fluentflat: 'yin-yang',    fluenthc: 'yin-yang',
+  },
+
+  '✂': {
+    apple:     '2702-fe0f',   google:    '2702-fe0f',
+    facebook:  '2702-fe0f',   twitter:   '2702-fe0f',
+    messenger: '2702-fe0f',
+    blobmoji:  'emoji_u2702', emojitwo:  '2702',
+    opencolor: '2702',        openblack: '2702',
+    oneui:     '2702',        whatsapp:  '2702',
+    fluent3d:  'scissors',    fluentflat: 'scissors',    fluenthc: 'scissors',
+  },
+
+  '☘': {
+    apple:     '2618-fe0f',   google:    '2618-fe0f',
+    facebook:  '2618-fe0f',   twitter:   '2618-fe0f',
+    messenger: '2618-fe0f',
+    blobmoji:  'emoji_u2618', emojitwo:  '2618',
+    opencolor: '2618',        openblack: '2618',
+    oneui:     '2618',        whatsapp:  '2618',
+    fluent3d:  'shamrock',    fluentflat: 'shamrock',    fluenthc: 'shamrock',
+  },
+
+  '✉': {
+    apple:     '2709-fe0f',   google:    '2709-fe0f',
+    facebook:  '2709-fe0f',   twitter:   '2709-fe0f',
+    messenger: '2709-fe0f',
+    blobmoji:  'emoji_u2709', emojitwo:  '2709',
+    opencolor: '2709',        openblack: '2709',
+    oneui:     '2709',        whatsapp:  '2709',
+    fluent3d:  'envelope',    fluentflat: 'envelope',    fluenthc: 'envelope',
+  },
+
+  '✔': {
+    apple:     '2714-fe0f',   google:    '2714-fe0f',
+    facebook:  '2714-fe0f',   twitter:   '2714-fe0f',
+    messenger: '2714-fe0f',
+    blobmoji:  'emoji_u2714', emojitwo:  '2714',
+    opencolor: '2714',        openblack: '2714',
+    oneui:     '2714',        whatsapp:  '2714',
+    fluent3d:  'check-mark',  fluentflat: 'check-mark',  fluenthc: 'check-mark',
+  },
+
+  '⚧': {
+    apple:     '26a7-fe0f',   google:    '26a7-fe0f',
+    facebook:  '26a7-fe0f',   twitter:   '26a7-fe0f',
+    messenger: '26a7-fe0f',
+    blobmoji:  'emoji_u26a7', emojitwo:  '26a7',
+    opencolor: '26A7',        openblack: '26A7',
+    oneui:     '26a7',        whatsapp:  '26a7',
+    fluent3d:  'transgender-symbol', fluentflat: 'transgender-symbol', fluenthc: 'transgender-symbol',
+  },
+
+  '✏': {
+    apple:     '270f-fe0f',   google:    '270f-fe0f',
+    facebook:  '270f-fe0f',   twitter:   '270f-fe0f',
+    messenger: '270f-fe0f',
+    blobmoji:  'emoji_u270f', emojitwo:  '270f',
+    opencolor: '270F',        openblack: '270F',
+    oneui:     '270f',        whatsapp:  '270f',
+    fluent3d:  'pencil',      fluentflat: 'pencil',      fluenthc: 'pencil',
+  },
+
+  '✒': {
+    apple:     '2712-fe0f',   google:    '2712-fe0f',
+    facebook:  '2712-fe0f',   twitter:   '2712-fe0f',
+    messenger: '2712-fe0f',
+    blobmoji:  'emoji_u2712', emojitwo:  '2712',
+    opencolor: '2712',        openblack: '2712',
+    oneui:     '2712',        whatsapp:  '2712',
+    fluent3d:  'black-nib',   fluentflat: 'black-nib',   fluenthc: 'black-nib',
+  },
+
+  '🗣': {
+    apple:     '1f5e3-fe0f',  google:    '1f5e3-fe0f',
+    facebook:  '1f5e3-fe0f',  twitter:   '1f5e3-fe0f',
+    messenger: '1f5e3-fe0f',
+    blobmoji:  'emoji_u1f5e3', emojitwo: '1f5e3',
+    opencolor: '1F5E3',       openblack: '1F5E3',
+    oneui:     '1f5e3',       whatsapp:  '1f5e3',
+    fluent3d:  'speaking-head', fluentflat: 'speaking-head', fluenthc: 'speaking-head',
+  },
+
+  '': {
+    apple:     '0030-fe0f-20e3', google:   '0030-fe0f-20e3',
+    facebook:  '0030-fe0f-20e3', twitter:  '0030-fe0f-20e3',
+    messenger: '0030-fe0f-20e3',
+    blobmoji:  'emoji_u0030_20e3', emojitwo: '0030-20e3',
+    opencolor: '0030-20E3',      openblack: '0030-20E3',
+    oneui:     '0030-20e3',      whatsapp:  '0030-20e3',
+    fluent3d:  'keycap-0',       fluentflat: 'keycap-0',  fluenthc: 'keycap-0',
+  },
+
+  '': {
+    apple:     '0031-fe0f-20e3', google:   '0031-fe0f-20e3',
+    facebook:  '0031-fe0f-20e3', twitter:  '0031-fe0f-20e3',
+    messenger: '0031-fe0f-20e3',
+    blobmoji:  'emoji_u0031_20e3', emojitwo: '0031-20e3',
+    opencolor: '0031-20E3',      openblack: '0031-20E3',
+    oneui:     '0031-20e3',      whatsapp:  '0031-20e3',
+    fluent3d:  'keycap-1',       fluentflat: 'keycap-1',  fluenthc: 'keycap-1',
+  },
+
+  '': {
+    apple:     '0032-fe0f-20e3', google:   '0032-fe0f-20e3',
+    facebook:  '0032-fe0f-20e3', twitter:  '0032-fe0f-20e3',
+    messenger: '0032-fe0f-20e3',
+    blobmoji:  'emoji_u0032_20e3', emojitwo: '0032-20e3',
+    opencolor: '0032-20E3',      openblack: '0032-20E3',
+    oneui:     '0032-20e3',      whatsapp:  '0032-20e3',
+    fluent3d:  'keycap-2',       fluentflat: 'keycap-2',  fluenthc: 'keycap-2',
+  },
+
+  '': {
+    apple:     '0033-fe0f-20e3', google:   '0033-fe0f-20e3',
+    facebook:  '0033-fe0f-20e3', twitter:  '0033-fe0f-20e3',
+    messenger: '0033-fe0f-20e3',
+    blobmoji:  'emoji_u0033_20e3', emojitwo: '0033-20e3',
+    opencolor: '0033-20E3',      openblack: '0033-20E3',
+    oneui:     '0033-20e3',      whatsapp:  '0033-20e3',
+    fluent3d:  'keycap-3',       fluentflat: 'keycap-3',  fluenthc: 'keycap-3',
+  },
+
+  '': {
+    apple:     '0034-fe0f-20e3', google:   '0034-fe0f-20e3',
+    facebook:  '0034-fe0f-20e3', twitter:  '0034-fe0f-20e3',
+    messenger: '0034-fe0f-20e3',
+    blobmoji:  'emoji_u0034_20e3', emojitwo: '0034-20e3',
+    opencolor: '0034-20E3',      openblack: '0034-20E3',
+    oneui:     '0034-20e3',      whatsapp:  '0034-20e3',
+    fluent3d:  'keycap-4',       fluentflat: 'keycap-4',  fluenthc: 'keycap-4',
+  },
+
+  '': {
+    apple:     '0035-fe0f-20e3', google:   '0035-fe0f-20e3',
+    facebook:  '0035-fe0f-20e3', twitter:  '0035-fe0f-20e3',
+    messenger: '0035-fe0f-20e3',
+    blobmoji:  'emoji_u0035_20e3', emojitwo: '0035-20e3',
+    opencolor: '0035-20E3',      openblack: '0035-20E3',
+    oneui:     '0035-20e3',      whatsapp:  '0035-20e3',
+    fluent3d:  'keycap-5',       fluentflat: 'keycap-5',  fluenthc: 'keycap-5',
+  },
+
+  '': {
+    apple:     '0036-fe0f-20e3', google:   '0036-fe0f-20e3',
+    facebook:  '0036-fe0f-20e3', twitter:  '0036-fe0f-20e3',
+    messenger: '0036-fe0f-20e3',
+    blobmoji:  'emoji_u0036_20e3', emojitwo: '0036-20e3',
+    opencolor: '0036-20E3',      openblack: '0036-20E3',
+    oneui:     '0036-20e3',      whatsapp:  '0036-20e3',
+    fluent3d:  'keycap-6',       fluentflat: 'keycap-6',  fluenthc: 'keycap-6',
+  },
+
+  '': {
+    apple:     '0037-fe0f-20e3', google:   '0037-fe0f-20e3',
+    facebook:  '0037-fe0f-20e3', twitter:  '0037-fe0f-20e3',
+    messenger: '0037-fe0f-20e3',
+    blobmoji:  'emoji_u0037_20e3', emojitwo: '0037-20e3',
+    opencolor: '0037-20E3',      openblack: '0037-20E3',
+    oneui:     '0037-20e3',      whatsapp:  '0037-20e3',
+    fluent3d:  'keycap-7',       fluentflat: 'keycap-7',  fluenthc: 'keycap-7',
+  },
+
+  '': {
+    apple:     '0038-fe0f-20e3', google:   '0038-fe0f-20e3',
+    facebook:  '0038-fe0f-20e3', twitter:  '0038-fe0f-20e3',
+    messenger: '0038-fe0f-20e3',
+    blobmoji:  'emoji_u0038_20e3', emojitwo: '0038-20e3',
+    opencolor: '0038-20E3',      openblack: '0038-20E3',
+    oneui:     '0038-20e3',      whatsapp:  '0038-20e3',
+    fluent3d:  'keycap-8',       fluentflat: 'keycap-8',  fluenthc: 'keycap-8',
+  },
+
+  '': {
+    apple:     '0039-fe0f-20e3', google:   '0039-fe0f-20e3',
+    facebook:  '0039-fe0f-20e3', twitter:  '0039-fe0f-20e3',
+    messenger: '0039-fe0f-20e3',
+    blobmoji:  'emoji_u0039_20e3', emojitwo: '0039-20e3',
+    opencolor: '0039-20E3',      openblack: '0039-20E3',
+    oneui:     '0039-20e3',      whatsapp:  '0039-20e3',
+    fluent3d:  'keycap-9',       fluentflat: 'keycap-9',  fluenthc: 'keycap-9',
+  },
+
+  '': {
+    apple:     '0023-fe0f-20e3', google:   '0023-fe0f-20e3',
+    facebook:  '0023-fe0f-20e3', twitter:  '0023-fe0f-20e3',
+    messenger: '0023-fe0f-20e3',
+    blobmoji:  'emoji_u0023_20e3', emojitwo: '0023-20e3',
+    opencolor: '0023-20E3',      openblack: '0023-20E3',
+    oneui:     '0023-20e3',      whatsapp:  '0023-20e3',
+    fluent3d:  'keycap-number-sign', fluentflat: 'keycap-number-sign', fluenthc: 'keycap-number-sign',
+  },
+
+  '': {
+    apple:     '002a-fe0f-20e3', google:   '002a-fe0f-20e3',
+    facebook:  '002a-fe0f-20e3', twitter:  '002a-fe0f-20e3',
+    messenger: '002a-fe0f-20e3',
+    blobmoji:  'emoji_u002a_20e3', emojitwo: '002a-20e3',
+    opencolor: '002A-20E3',      openblack: '002A-20E3',
+    oneui:     '002a-20e3',      whatsapp:  '002a-20e3',
+    fluent3d:  'keycap-asterisk', fluentflat: 'keycap-asterisk', fluenthc: 'keycap-asterisk',
+  },
+
+};
+
+module.exports = overrides;
