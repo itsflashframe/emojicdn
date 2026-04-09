26345ced01f4051bb5785cb8c753f94268c8be53
itsflashframe <268517099+itsflashframe@users.noreply.github.com>
2026-04-09 10:20:11 +0100

    Update sourcechange.js

--- api/sourcechange.js
+++ api/sourcechange.js
@@ -5,6 +5,8 @@ const sourceChanges = {
 
   '1faea': {
   google: 'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1faea.svg',
+   apple: 'https://em-content.zobj.net/source/apple/453/distorted-face_1faea.png'
+    
 
 }
 
