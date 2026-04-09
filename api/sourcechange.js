7544023c487a6788da11e312b1164ea933f557b4
itsflashframe <268517099+itsflashframe@users.noreply.github.com>
2026-04-09 06:57:51 +0100

    Add files via upload

---
+++ api/sourcechange.js
@@ -0,0 +1,13 @@
+
+
+
+const sourceChanges = {
+
+  '1faea': {
+  google: 'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1faea.svg',
+
+}
+
+};
+
+module.exports = sourceChanges;
