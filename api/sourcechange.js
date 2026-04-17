127d4e836e1c7216aa2acff2a02859a09d61898c
itsflashframe <268517099+itsflashframe@users.noreply.github.com>
2026-04-17 02:25:43 +0100

    Update sourcechange.js

--- api/sourcechange.js
+++ api/sourcechange.js
@@ -1,151 +1,134 @@
+const ET17 = 'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg';
+const NOTO = 'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg';
+const NPM  = 'https://cdn.jsdelivr.net/npm/fluentui-emoji@1.3.0/icons';
+
 const sourceChanges = {
 
   '1faea': {
-    google:   'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1faea.svg',
-    apple:    'https://em-content.zobj.net/source/apple/453/distorted-face_1faea.png',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Distorted%20Face/3D/distorted_face_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Distorted%20Face/Flat/distorted_face_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Distorted%20Face/High%20Contrast/distorted_face_high_contrast.svg',
+    apple: 'https://em-content.zobj.net/source/apple/453/distorted-face_1faea.png',
+    fluent3d:   `${ET17}/1faea.svg`,
+    fluentflat: `${ET17}/1faea.svg`,
+    fluenthc:   `${ET17}/1faea.svg`,
   },
 
   '1faef': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1faef.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Fight%20Cloud/3D/fight_cloud_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Fight%20Cloud/Flat/fight_cloud_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Fight%20Cloud/High%20Contrast/fight_cloud_high_contrast.svg',
+    fluent3d:   `${ET17}/1faef.svg`,
+    fluentflat: `${ET17}/1faef.svg`,
+    fluenthc:   `${ET17}/1faef.svg`,
   },
 
   '1fac8': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1fac8.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Hairy%20Creature/3D/hairy_creature_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Hairy%20Creature/Flat/hairy_creature_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Hairy%20Creature/High%20Contrast/hairy_creature_high_contrast.svg',
+    fluent3d:   `${ET17}/1fac8.svg`,
+    fluentflat: `${ET17}/1fac8.svg`,
+    fluenthc:   `${ET17}/1fac8.svg`,
   },
 
   '1f9d1-200d-1fa70': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1f9d1-200d-1fa70.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Ballet%20Dancer/3D/ballet_dancer_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Ballet%20Dancer/Flat/ballet_dancer_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Ballet%20Dancer/High%20Contrast/ballet_dancer_high_contrast.svg',
+    fluent3d:   `${NOTO}/emoji_u1f9d1_200d_1fa70.svg`,
+    fluentflat: `${NOTO}/emoji_u1f9d1_200d_1fa70.svg`,
+    fluenthc:   `${NOTO}/emoji_u1f9d1_200d_1fa70.svg`,
   },
 
   '1f46f': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1f46f.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/People%20with%20bunny%20ears/3D/people_with_bunny_ears_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/People%20with%20bunny%20ears/Flat/people_with_bunny_ears_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/People%20with%20bunny%20ears/High%20Contrast/people_with_bunny_ears_high_contrast.svg',
+    fluent3d:   `${ET17}/1f46f.svg`,
+    fluentflat: `${ET17}/1f46f.svg`,
+    fluenthc:   `${ET17}/1f46f.svg`,
   },
 
   '1f93c': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1f93c.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/People%20wrestling/3D/people_wrestling_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/People%20wrestling/Flat/people_wrestling_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/People%20wrestling/High%20Contrast/people_wrestling_high_contrast.svg',
+    fluent3d:   `${ET17}/1f93c.svg`,
+    fluentflat: `${ET17}/1f93c.svg`,
+    fluenthc:   `${ET17}/1f93c.svg`,
   },
 
   '1facd': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1facd.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Orca/3D/orca_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Orca/Flat/orca_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Orca/High%20Contrast/orca_high_contrast.svg',
+    fluent3d:   `${ET17}/1facd.svg`,
+    fluentflat: `${ET17}/1facd.svg`,
+    fluenthc:   `${ET17}/1facd.svg`,
   },
 
   '1f6d8': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1f6d8.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Landslide/3D/landslide_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Landslide/Flat/landslide_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Landslide/High%20Contrast/landslide_high_contrast.svg',
+    fluent3d:   `${ET17}/1f6d8.svg`,
+    fluentflat: `${ET17}/1f6d8.svg`,
+    fluenthc:   `${ET17}/1f6d8.svg`,
   },
 
   '1fa8a': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1fa8a.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Trombone/3D/trombone_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Trombone/Flat/trombone_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Trombone/High%20Contrast/trombone_high_contrast.svg',
+    fluent3d:   `${ET17}/1fa8a.svg`,
+    fluentflat: `${ET17}/1fa8a.svg`,
+    fluenthc:   `${ET17}/1fa8a.svg`,
   },
 
   '1fa8e': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1fa8e.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Treasure%20Chest/3D/treasure_chest_3d.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Treasure%20Chest/Flat/treasure_chest_flat.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/gh/microsoft/fluentui-emoji@main/assets/Treasure%20Chest/High%20Contrast/treasure_chest_high_contrast.svg',
+    fluent3d:   `${ET17}/1fa8e.svg`,
+    fluentflat: `${ET17}/1fa8e.svg`,
+    fluenthc:   `${ET17}/1fa8e.svg`,
   },
 
   '1fae9': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/svg/1fae9.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/face-with-bags-under-eyes.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/face-with-bags-under-eyes.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/face-with-bags-under-eyes.svg',
+    fluent3d:   `${NOTO}/emoji_u1fae9.svg`,
+    fluentflat: `${NOTO}/emoji_u1fae9.svg`,
+    fluenthc:   `${NOTO}/emoji_u1fae9.svg`,
   },
 
   '1f426-200d-1f525': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1f426_200d_1f525.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/phoenix.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/phoenix.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/phoenix.svg',
+    fluent3d:   `${NOTO}/emoji_u1f426_200d_1f525.svg`,
+    fluentflat: `${NOTO}/emoji_u1f426_200d_1f525.svg`,
+    fluenthc:   `${NOTO}/emoji_u1f426_200d_1f525.svg`,
   },
 
   '1f426-200d-2b1b': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1f426_200d_2b1b.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/black-bird.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/black-bird.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/black-bird.svg',
+    fluent3d:   `${NPM}/modern/blackbird.svg`,
+    fluentflat: `${NPM}/flat/blackbird.svg`,
+    fluenthc:   `${NPM}/high-contrast/blackbird.svg`,
   },
 
   '1f34b-200d-1f7e9': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1f34b_200d_1f7e9.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/lime.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/lime.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/lime.svg',
+    fluent3d:   `${NPM}/modern/lime.svg`,
+    fluentflat: `${NPM}/flat/lime.svg`,
+    fluenthc:   `${NPM}/high-contrast/lime.svg`,
   },
 
   '1f344-200d-1f7eb': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/svg/1f344-200d-1f7eb.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/brown-mushroom.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/brown-mushroom.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/brown-mushroom.svg',
+    fluent3d:   `${NPM}/modern/brown-mushroom.svg`,
+    fluentflat: `${NPM}/flat/brown-mushroom.svg`,
+    fluenthc:   `${NPM}/high-contrast/brown-mushroom.svg`,
   },
 
   '1fabf': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1fabf.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/goose.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/goose.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/goose.svg',
+    fluent3d:   `${NPM}/modern/goose.svg`,
+    fluentflat: `${NPM}/flat/goose.svg`,
+    fluenthc:   `${NPM}/high-contrast/goose.svg`,
   },
 
   '1face': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/svg/1face.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/moose.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/moose.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/moose.svg',
+    fluent3d:   `${NPM}/modern/moose.svg`,
+    fluentflat: `${NPM}/flat/moose.svg`,
+    fluenthc:   `${NPM}/high-contrast/moose.svg`,
   },
 
   '1facf': {
-    google:     'https://images.emojiterra.com/google/noto-emoji/unicode-16.0/color/svg/1facf.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/donkey.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/donkey.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/donkey.svg',
+    fluent3d:   `${NPM}/modern/donkey.svg`,
+    fluentflat: `${NPM}/flat/donkey.svg`,
+    fluenthc:   `${NPM}/high-contrast/donkey.svg`,
   },
 
   '1fab8': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1fab8.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/jellyfish.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/jellyfish.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/jellyfish.svg',
+    fluent3d:   `${NPM}/modern/jellyfish.svg`,
+    fluentflat: `${NPM}/flat/jellyfish.svg`,
+    fluenthc:   `${NPM}/high-contrast/jellyfish.svg`,
   },
 
   '1faf7': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1faf7.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/leftwards-pushing-hand.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/leftwards-pushing-hand.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/leftwards-pushing-hand.svg',
+    fluent3d:   `${NOTO}/emoji_u1faf7.svg`,
+    fluentflat: `${NOTO}/emoji_u1faf7.svg`,
+    fluenthc:   `${NOTO}/emoji_u1faf7.svg`,
   },
 
   '1faf8': {
-    google:     'https://cdn.jsdelivr.net/gh/googlefonts/noto-emoji@main/svg/emoji_u1faf8.svg',
-    fluent3d:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/modern/rightwards-pushing-hand.svg',
-    fluentflat: 'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/flat/rightwards-pushing-hand.svg',
-    fluenthc:   'https://cdn.jsdelivr.net/npm/fluentui-emoji@latest/icons/high-contrast/rightwards-pushing-hand.svg',
+    fluent3d:   `${NOTO}/emoji_u1faf8.svg`,
+    fluentflat: `${NOTO}/emoji_u1faf8.svg`,
+    fluenthc:   `${NOTO}/emoji_u1faf8.svg`,
   },
 
   '1f3f4-e0067-e0062-e0065-e006e-e0067-e007f': {
